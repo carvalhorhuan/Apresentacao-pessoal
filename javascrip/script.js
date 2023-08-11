dbd5566f8193f88ba4b63108f4cb21851e3a92fe
Rhuan Carvalho <carvalho.rhuan@gmail.com>
2023-08-11 16:49:54 -0300

    Texto About Escondido com interação JS

--- javascrip/script.js
+++ javascrip/script.js
@@ -6,3 +6,17 @@ function menushow(){
         menumobile.classlist.add('open');
     }
 }
+
+var span = document.querySelector('.texto-pessoal span');
+
+span.addEventListener('click',function(){
+    var conteudo = document.querySelector('.texto-pessoal p');
+
+    if(conteudo.classList.contains('mostrar')){
+        span.innerHTML = 'Ver Mais!'
+        conteudo.classList.remove('mostrar');
+    }else{
+        span.innerHTML = 'Ocultar!'
+        conteudo.classList.add('mostrar');
+    }
+});
